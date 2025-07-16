dfe559ca82d6a8e0ade80ee363538df9a8e5a0d0
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-07-16 03:25:16 +0300

    upd

--- php/BuildPhp/config/vc15/x64/config.ts.bat
+++ php/BuildPhp/config/vc15/x64/config.ts.bat
@@ -1,1 +1,1 @@
-cscript /nologo configure.js "--enable-snapshot-build" "--enable-debug-pack" "--enable-object-out-dir=../obj/" "--enable-com-dotnet=shared" "--with-oci8=..\oracle\instantclient_10_2" "--with-pdo-oci=..\oracle\instantclient_10_2" "--without-analyzer" "--enable-pgi" %*
+cscript /nologo configure.js "--enable-snapshot-build" "--enable-debug-pack" "--enable-object-out-dir=../obj/" "--enable-com-dotnet=shared" "--without-analyzer" "--enable-pgi" %*
