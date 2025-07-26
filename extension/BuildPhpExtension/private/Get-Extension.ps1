64aaf605cd6d7e9d363a8d0c82cd5836ed6c6aad
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-07-26 17:31:58 +0300

    Update Get-Extension.ps1

--- extension/BuildPhpExtension/private/Get-Extension.ps1
+++ extension/BuildPhpExtension/private/Get-Extension.ps1
@@ -76,7 +76,7 @@ function Get-Extension {
 
                     if (-not $name) { throw "<name> tag not found in XML" }
                     if ($name -eq $currentDirectoryName) { return }
-
+                    if ($name -eq "datadog_trace") { $name = "ddtrace" }
                     $newPath = Join-Path $parentDirectory $name
                     if (Test-Path $newPath) { throw "Target folder already exists: $newPath" }
 
