fa8e0dde99b47cb27246aa89bb1425e8994a7832
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-07-27 03:49:40 +0300

    Update Get-Extension.ps1

--- extension/BuildPhpExtension/private/Get-Extension.ps1
+++ extension/BuildPhpExtension/private/Get-Extension.ps1
@@ -61,9 +61,9 @@ function Get-Extension {
                     git remote add origin $ExtensionUrl > $null 2>&1
                     git fetch --depth=1 origin $ExtensionRef > $null 2>&1
                     git checkout FETCH_HEAD > $null 2>&1
-                    # if($Extension.Contains("ddtrace")) {
-                    #   git submodule update --init --recursive > $null 2>&1
-                    #}
+                    if($Extension.Contains("ddtrace")) {
+                      git submodule update --init --recursive > $null 2>&1
+                    }
                 }
             }
 
