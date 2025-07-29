4790d601e9a6195954abccaef8c1b98b15d3907d
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-07-29 06:16:44 +0300

    Update patches

--- extension/BuildPhpExtension/patches/crypto.ps1
+++ extension/BuildPhpExtension/patches/crypto.ps1
@@ -1,2 +1,3 @@
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bukka/php-crypto/4516e22160a32ea09b2e547ceebd9a009fc6b597/crypto_cipher.c" -OutFile "crypto_cipher.c"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bukka/php-crypto/4516e22160a32ea09b2e547ceebd9a009fc6b597/crypto_hash.c" -OutFile "crypto_hash.c"
+mkdir phpc -Force | Out-Null; Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bukka/phpc/master/phpc.h" -OutFile "phpc\phpc.h"
