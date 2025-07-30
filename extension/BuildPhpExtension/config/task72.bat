add676f21207dd2da9f2912691aa94bdb86a3bbc
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-07-30 05:10:51 +0300

    Update task72.bat

--- extension/BuildPhpExtension/config/task72.bat
+++ extension/BuildPhpExtension/config/task72.bat
@@ -1,5 +1,5 @@
 
 call phpize 2>&1
-call configure "--with-php-build=..\deps" OPTIONS "--enable-fd-setsize=8192" "--with-odbcver=0x0380" "--enable-com-dotnet=shared" "--without-analyzer" "--with-mp=disable" 2>&1
+call configure "--with-php-build=..\deps" OPTIONS "--with-mp=disable" 2>&1
 nmake /nologo 2>&1
 exit %errorlevel%
