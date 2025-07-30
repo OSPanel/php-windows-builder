d99c0752ae983cc044824153bc9790b2eacd6b9e
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-07-30 04:22:42 +0300

    Update Add-Boost.ps1

--- extension/BuildPhpExtension/private/Add-Boost.ps1
+++ extension/BuildPhpExtension/private/Add-Boost.ps1
@@ -9,9 +9,17 @@ Function Add-Boost {
     begin {
     }
     process {
-        $url = "https://archives.boost.io/release/1.72.0/source/boost_1_72_0.zip"
+        $url = "https://archives.boost.io/release/1.88.0/source/boost_1_88_0.zip"
         Invoke-WebRequest $url -OutFile "boost.zip"
         Expand-Archive -Path "boost.zip" -DestinationPath "../deps"
+        if (-not (Test-Path "../deps/boost")) {
+            New-Item -Path "../deps/boost" -ItemType Directory | Out-Null
+        }
+        if (-not (Test-Path "./boost")) {
+            New-Item -Path "./boost" -ItemType Directory | Out-Null
+        }
+        Copy-Item -Path "../deps/boost_1_88_0/*" -Destination "../deps/boost" -Recurse
+        Copy-Item -Path "../deps/boost_1_88_0/*" -Destination "./boost" -Recurse
     }
     end {
     }
