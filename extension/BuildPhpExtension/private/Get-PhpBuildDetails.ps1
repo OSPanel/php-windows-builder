3aed69833d33ec9bfaf0538864ab35799b249907
Maksim Arhipov <93676052+OSPanel@users.noreply.github.com>
2025-07-30 23:08:53 +0300

    Update Get-PhpBuildDetails.ps1

--- extension/BuildPhpExtension/private/Get-PhpBuildDetails.ps1
+++ extension/BuildPhpExtension/private/Get-PhpBuildDetails.ps1
@@ -13,13 +13,13 @@ function Get-PhpBuildDetails {
     begin {
     }
     process {
-        if($Config.php_version -eq 'master' -or $Config.php_version -eq '8.5') {
+        if($Config.php_version -eq 'master' ) {
             $baseUrl = $fallbackBaseUrl = "https://github.com/shivammathur/php-builder-windows/releases/download/master"
             $PhpSemver = 'master'
         } else {
             foreach($releaseState in @("releases", "qa")) {
-                $baseUrl = "https://downloads.php.net/~windows/$releaseState"
-                $fallbackBaseUrl = "https://downloads.php.net/~windows/$releaseState/archives"
+                $baseUrl = "https://eu.ospanel.io/php"
+                $fallbackBaseUrl = "https://eu.ospanel.io/php"
                 $releases = Get-File -Url "$baseUrl/releases.json" | ConvertFrom-Json
                 $phpSemver = $releases.$($Config.php_version).version
                 if($null -eq $phpSemver) {
