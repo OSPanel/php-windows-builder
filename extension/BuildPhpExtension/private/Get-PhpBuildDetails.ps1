6f9ddb2246ddc989b4794d9f37c1c347184deda0
Maksim Arhipov <93676052+OSPanel@users.noreply.github.com>
2025-07-30 21:35:20 +0300

    Update Get-PhpBuildDetails.ps1

--- extension/BuildPhpExtension/private/Get-PhpBuildDetails.ps1
+++ extension/BuildPhpExtension/private/Get-PhpBuildDetails.ps1
@@ -13,14 +13,26 @@ function Get-PhpBuildDetails {
     begin {
     }
     process {
-        if($Config.php_version -eq 'master') {
+        if($Config.php_version -eq 'master' -or $Config.php_version -eq '8.5') {
             $baseUrl = $fallbackBaseUrl = "https://github.com/shivammathur/php-builder-windows/releases/download/master"
             $PhpSemver = 'master'
         } else {
-                $baseUrl = $fallbackBaseUrl = "https://eu.ospanel.io/php"
-                # $baseUrl = "https://downloads.php.net/~windows/releases"
-                $releases = Invoke-WebRequest "$baseUrl/releases.json" | ConvertFrom-Json
+            foreach($releaseState in @("releases", "qa")) {
+                $baseUrl = "https://eu.ospanel.io/php"
+                $fallbackBaseUrl = "https://eu.ospanel.io/php"
+                $releases = Get-File -Url "$baseUrl/releases.json" | ConvertFrom-Json
                 $phpSemver = $releases.$($Config.php_version).version
+                if($null -eq $phpSemver) {
+                    $phpSemver = (Get-File -Url $fallbackBaseUrl).Links |
+                            Where-Object { $_.href -match "php-($($Config.php_version).[0-9]+).*" } |
+                            ForEach-Object { $matches[1] } |
+                            Sort-Object { [System.Version]$_ } -Descending |
+                            Select-Object -First 1
+                }
+                if($null -ne $phpSemver) {
+                    break
+                }
+            }
         }
         return [PSCustomObject]@{
             phpSemver = $phpSemver
