140090fc73ab8e2ed8294f36754c9eac8c7deb2e
Maksim Arhipov <93676052+OSPanel@users.noreply.github.com>
2025-08-03 09:27:30 +0300

    Update Get-PhpSdk.ps1

--- extension/BuildPhpExtension/private/Get-PhpSdk.ps1
+++ extension/BuildPhpExtension/private/Get-PhpSdk.ps1
@@ -21,7 +21,7 @@ function Get-PhpSdk {
             $sdkZipFilePath = Join-Path $currentDirectory php-sdk.zip
             [System.IO.Compression.ZipFile]::ExtractToDirectory($sdkZipFilePath, $currentDirectory)
             Rename-Item -Path php-sdk-binary-tools-php-sdk-$sdkVersion php-sdk
-
+            Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OSPanel/php-windows-builder/refs/heads/master/resources/Series.php" -OutFile "php-sdk\lib\php\libsdk\SDK\Build\Dependency\Series.php"
             $sdkDirectoryPath = Join-Path $currentDirectory php-sdk
             $sdkBinDirectoryPath = Join-Path $sdkDirectoryPath bin
             $sdkMsys2DirectoryPath = Join-Path $sdkDirectoryPath msys2
