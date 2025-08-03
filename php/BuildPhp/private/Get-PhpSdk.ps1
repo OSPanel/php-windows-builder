7ae15d808bbda436db1128a06ba08d0b9c14264d
Maksim Arhipov <93676052+OSPanel@users.noreply.github.com>
2025-08-03 11:37:55 +0300

    Update Get-PhpSdk.ps1

--- php/BuildPhp/private/Get-PhpSdk.ps1
+++ php/BuildPhp/private/Get-PhpSdk.ps1
@@ -8,13 +8,12 @@ function Get-PhpSdk {
     )
     begin {
         $sdkVersion = "2.4.0"
-        $url = "https://github.com/php/php-sdk-binary-tools/archive/php-sdk-$sdkVersion.zip"
+        $url = "https://eu.ospanel.io/php-sdk-$sdkVersion.zip"
     }
     process {
         Get-File -Url $url -OutFile php-sdk.zip
         Expand-Archive -Path php-sdk.zip -DestinationPath .
         Rename-Item -Path php-sdk-binary-tools-php-sdk-$sdkVersion php-sdk
-        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OSPanel/php-windows-builder/refs/heads/master/resources/Series.php" -OutFile "php-sdk\lib\php\libsdk\SDK\Build\Dependency\Series.php"
     }
     end {
     }
