3ce1d21ea155ea0c04b080afa2ebfe0cbb158e9a
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-08-03 06:51:40 +0300

    Update brotli.ps1

--- extension/BuildPhpExtension/patches/brotli.ps1
+++ extension/BuildPhpExtension/patches/brotli.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri "https://eu.ospanel.io/ext/brotli_config.w32" -OutFile "config.w32"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/OSPanel/php-windows-builder/refs/heads/master/resources/brotli_config.w32" -OutFile "config.w32"
