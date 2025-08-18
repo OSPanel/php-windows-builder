e30910b14b05bb15d3465fd4b65d127a86f267a1
OSPanel <93676052+OSPanel@users.noreply.github.com>
2025-08-19 01:16:38 +0300

    Update Get-Extension.ps1

--- extension/BuildPhpExtension/private/Get-Extension.ps1
+++ extension/BuildPhpExtension/private/Get-Extension.ps1
@@ -32,179 +32,196 @@ function Get-Extension {
     begin {}
 
     process {
-        Add-StepLog "Fetching extension from $ExtensionUrl-$ExtensionRef.tgz"
-        try {
-            if (
-                ($null -eq $ExtensionUrl -or $null -eq $ExtensionRef) -or
-                ($ExtensionUrl -eq '' -or $ExtensionRef -eq '')
-            ) {
-                throw "Both Extension URL and Extension Reference are required."
-            }
-
+        if ($LocalSrc) {
             $currentDirectory = (Get-Location).Path
-            $Extension = Split-Path -Path $ExtensionUrl -Leaf
-            $extension_orig = $Extension
-
-            if ($Extension.Contains("dd-trace-php")) { $Extension = "ddtrace" }
-            if ($Extension.Contains("datadog_trace")) { $Extension = "ddtrace" }
-            if ($Extension.Contains("libsodium")) { $Extension = "sodium" }
-            if ($Extension.Contains("oci8")) { $Extension = "oci8_19" }
-            if ($Extension.Contains("pdo_oci")) { $Extension = "pdo_oci" }
-            if ($Extension.Contains("apcu_bc")) { $Extension = "apc" }
-
-            $extensionPath = Join-Path -Path $currentDirectory -ChildPath $Extension
-
-            if (-not (Test-Path $extensionPath)) {
-                New-Item -Path $extensionPath -ItemType Directory | Out-Null
-            }
-
-            Set-Location -Path $extensionPath
-            $currentDirectory = (Get-Location).Path
-
-            if ($null -ne $ExtensionUrl -and $null -ne $ExtensionRef) {
-                if ($ExtensionUrl -like "*pecl.php.net*") {
-                    try {
-                        Get-File -Url "https://pecl.php.net/get/$extension_orig-$ExtensionRef.tgz" -OutFile "$extension_orig-$ExtensionRef.tgz"
-                    } catch {}
-
-                    if (-not (Test-Path "$extension_orig-$ExtensionRef.tgz")) {
-                        try {
-                            Get-File -Url "https://pecl.php.net/get/$($extension_orig.ToUpper())-$ExtensionRef.tgz" -OutFile "$extension_orig-$ExtensionRef.tgz"
-                        } catch {}
-                    }
-
-                    & tar -xzf "$extension_orig-$ExtensionRef.tgz" -C $currentDirectory
-                    Copy-Item -Path "$extension_orig-$ExtensionRef\*" -Destination $currentDirectory -Recurse -Force
-                    Remove-Item -Path "$extension_orig-$ExtensionRef" -Recurse -Force
+            Copy-Item -Path "$currentDirectory\*" -Destination $BuildDirectory -Recurse -Force
+        }
+        else {
+            Add-StepLog "Fetching extension from $ExtensionUrl"
+            try {
+                if (
+                    ($null -eq $ExtensionUrl -or $null -eq $ExtensionRef) -or
+                    ($ExtensionUrl -eq '' -or $ExtensionRef -eq '')
+                ) {
+                    throw "Both Extension URL and Extension Reference are required."
                 }
-                else {
-                    if ($null -ne $env:AUTH_TOKEN) {
-                        $ExtensionUrl = $ExtensionUrl -replace '^https://', "https://${Env:AUTH_TOKEN}@"
-                    }
 
-                    git init > $null 2>&1
-                    git remote add origin $ExtensionUrl > $null 2>&1
+                Set-Location $BuildDirectory
+                $currentDirectory = (Get-Location).Path
+                $Extension = Split-Path -Path $ExtensionUrl -Leaf
+                $extension_orig = $Extension
 
-                    if ($Extension -in @("oci8_19", "pdo_oci")) {
-                        git fetch --depth=1 origin main > $null 2>&1
+                if ($Extension.Contains("dd-trace-php")) { $Extension = "ddtrace" }
+                if ($Extension.Contains("datadog_trace"))    { $Extension = "ddtrace" }
+                if ($Extension.Contains("libsodium"))        { $Extension = "sodium" }
+                if ($Extension.Contains("oci8"))             { $Extension = "oci8_19" }
+                if ($Extension.Contains("pdo_oci"))          { $Extension = "pdo_oci" }
+                if ($Extension.Contains("apcu_bc"))          { $Extension = "apc" }
+
+                $extensionPath = Join-Path -Path $currentDirectory -ChildPath $Extension
+
+                if (-not (Test-Path $extensionPath)) {
+                    New-Item -Path $extensionPath -ItemType Directory | Out-Null
+                }
+
+                Set-Location -Path $extensionPath
+                $currentDirectory = (Get-Location).Path
+
+                if ($null -ne $ExtensionUrl -and $null -ne $ExtensionRef) {
+                    if ($ExtensionUrl -like "*pecl.php.net*") {
+                        try {
+                            Get-File -Url "https://pecl.php.net/get/$extension_orig-$ExtensionRef.tgz" -OutFile "$extension_orig-$ExtensionRef.tgz"
+                        } catch {}
+
+                        if (-not (Test-Path "$extension_orig-$ExtensionRef.tgz")) {
+                            try {
+                                Get-File -Url "https://pecl.php.net/get/$($extension_orig.ToUpper())-$ExtensionRef.tgz" -OutFile "$extension_orig-$ExtensionRef.tgz"
+                            } catch {}
+                        }
+
+                        & tar -xzf "$extension_orig-$ExtensionRef.tgz" -C $currentDirectory
+                        Copy-Item -Path "$extension_orig-$ExtensionRef\*" -Destination $currentDirectory -Recurse -Force
+                        Remove-Item -Path "$extension_orig-$ExtensionRef" -Recurse -Force
                     }
                     else {
-                        git fetch --depth=1 origin $ExtensionRef > $null 2>&1
-                    }
+                        if ($null -ne $env:AUTH_TOKEN) {
+                            $ExtensionUrl = $ExtensionUrl -replace '^https://', "https://${Env:AUTH_TOKEN}@"
+                        }
 
-                    git checkout FETCH_HEAD > $null 2>&1
+                        git init > $null 2>&1
+                        git remote add origin $ExtensionUrl > $null 2>&1
 
-                    $targetExtensions = @("ddtrace", "lz4")
-                    if ($targetExtensions | Where-Object { $Extension.Contains($_) }) {
-                        git submodule update --init --recursive > $null 2>&1
+                        if ($Extension -in @("oci8_19", "pdo_oci")) {
+                            git fetch --depth=1 origin main > $null 2>&1
+                        }
+                        else {
+                            git fetch --depth=1 origin $ExtensionRef > $null 2>&1
+                        }
+
+                        git checkout FETCH_HEAD > $null 2>&1
+
+                        $targetExtensions = @("ddtrace", "lz4")
+                        if ($targetExtensions | Where-Object { $Extension.Contains($_) }) {
+                            git submodule update --init --recursive > $null 2>&1
+                        }
                     }
                 }
-            }
 
-            & {
-                try {
+                & {
+                    try {
+                        $currentDirectory = (Get-Location).Path
+                        $currentDirectoryName = Split-Path $currentDirectory -Leaf
+                        $parentDirectory = Split-Path $currentDirectory -Parent
+                        $xmlPath = Join-Path $currentDirectory "package.xml"
+
+                        if (-not (Test-Path $xmlPath)) { throw "package.xml not found at $xmlPath" }
+
+                        [xml]$xml = Get-Content $xmlPath
+                        $ns = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
+                        $ns.AddNamespace("p", "http://pear.php.net/dtd/package-2.0")
+
+                        $name = $xml.SelectSingleNode("//p:name", $ns).InnerText
+                        if (-not $name) { throw "<name> tag not found in XML" }
+
+                        if ($name -eq "datadog_trace") { $name = "ddtrace" }
+                        if ($name -eq "oci8")          { $name = $Extension }
+                        if ($name -eq "apcu_bc")       { $name = $Extension }
+
+                        if ($name -eq $currentDirectoryName) { return }
+
+                        $newPath = Join-Path $parentDirectory $name
+                        if (Test-Path $newPath) { throw "Target folder already exists: $newPath" }
+
+                        Set-Location $parentDirectory
+                        Rename-Item -Path $currentDirectory -NewName $name
+
+                        Write-Host "Renamed folder:`n$currentDirectory`n>`n$newPath"
+                        Set-Location $newPath
+                    }
+                    catch {
+                        Write-Host "Skipping rename: $_"
+                    }
+                }
+
+                if ($Extension.Contains("lz4")) {
                     $currentDirectory = (Get-Location).Path
-                    $currentDirectoryName = Split-Path $currentDirectory -Leaf
                     $parentDirectory = Split-Path $currentDirectory -Parent
-                    $xmlPath = Join-Path $currentDirectory "package.xml"
-
-                    if (-not (Test-Path $xmlPath)) { throw "package.xml not found at $xmlPath" }
-
-                    [xml]$xml = Get-Content $xmlPath
-                    $ns = New-Object System.Xml.XmlNamespaceManager($xml.NameTable)
-                    $ns.AddNamespace("p", "http://pear.php.net/dtd/package-2.0")
-
-                    $name = $xml.SelectSingleNode("//p:name", $ns).InnerText
-                    if (-not $name) { throw "<name> tag not found in XML" }
-
-                    if ($name -eq "datadog_trace") { $name = "ddtrace" }
-                    if ($name -eq "oci8") { $name = $Extension }
-                    if ($name -eq "apcu_bc") { $name = $Extension }
-
-                    if ($name -eq $currentDirectoryName) { return }
-
-                    $newPath = Join-Path $parentDirectory $name
-                    if (Test-Path $newPath) { throw "Target folder already exists: $newPath" }
-
                     Set-Location $parentDirectory
-                    Rename-Item -Path $currentDirectory -NewName $name
-
-                    Write-Host "Renamed folder:`n$currentDirectory`n>`n$newPath"
-                    Set-Location $newPath
-                }
-                catch {
-                    Write-Host "Skipping rename: $_"
+                    Rename-Item -Path $currentDirectory -NewName lz4
+                    Set-Location lz4
                 }
             }
-
-            if ($Extension.Contains("lz4")) {
-                $currentDirectory = (Get-Location).Path
-                $parentDirectory = Split-Path $currentDirectory -Parent
-                Set-Location $parentDirectory
-                Rename-Item -Path $currentDirectory -NewName lz4
-                Set-Location lz4
+            catch {
+                Add-BuildLog cross extension "Failed to fetch extension from $ExtensionUrl"
+                throw
             }
+        }
 
-            $currentDirectory = (Get-Location).Path
-            $Extension = Split-Path -Path (Get-Location) -Leaf
-            $patches = $False
+        $currentDirectory = (Get-Location).Path
+        $Extension = Split-Path -Path (Get-Location) -Leaf
+        $patches = $false
 
+        if ($null -ne $Extension) {
             if (Test-Path -Path "$PSScriptRoot\..\patches\$Extension.ps1") {
                 Add-Patches $Extension
-                $patches = $True
+                $patches = $true
             }
 
             if (Test-Path -Path "$PSScriptRoot\..\patches\$Extension-$ExtensionRef.ps1") {
                 Add-Patches "$Extension-$ExtensionRef"
-                $patches = $True
+                $patches = $true
             }
+        }
 
-            $configW32 = Get-ChildItem (Get-Location).Path -Recurse -Filter "config.w32" -ErrorAction SilentlyContinue
-            if ($null -eq $configW32) {
-                throw "No config.w32 found"
+        $configW32 = Get-ChildItem (Get-Location).Path -Recurse -Filter "config.w32" -ErrorAction SilentlyContinue
+        if ($null -eq $configW32) {
+            if ($LocalSrc) {
+                throw "No config.w32 found, please make sure you are in the extension source directory and it supports Windows."
             }
+            else {
+                throw "No config.w32 found, please check if the extension supports Windows."
+            }
+        }
 
-            $subDirectory = $configW32.DirectoryName
-            if ((Get-Location).Path -ne $subDirectory) {
-                Copy-Item -Path "${subDirectory}\*" -Destination $currentDirectory -Recurse -Force
-                Remove-Item -Path $subDirectory -Recurse -Force
-            }
+        $subDirectory = $configW32.DirectoryName
+        if ((Get-Location).Path -ne $subDirectory) {
+            Copy-Item -Path "${subDirectory}\*" -Destination $currentDirectory -Recurse -Force
+            Remove-Item -Path $subDirectory -Recurse -Force
+        }
 
-            $configW32Content = Get-Content -Path "config.w32"
-            $extensionLine = $configW32Content | Select-String -Pattern '\s+(ZEND_)?EXTENSION\(' | Select-Object -Last 1
+        $configW32Content = Get-Content -Path "config.w32"
+        $extensionLine = $configW32Content | Select-String -Pattern '\s+(ZEND_)?EXTENSION\(' | Select-Object -Last 1
 
-            if ($null -eq $extensionLine) {
-                throw "No extension found in config.w32"
-            }
+        if ($null -eq $extensionLine) {
+            throw "No extension found in config.w32"
+        }
 
-            $name = ($extensionLine -replace '.*EXTENSION\(([^,]+),.*', '$1') -replace '["'']', ''
+        $name = ($extensionLine -replace '.*EXTENSION\(([^,]+),.*', '$1') -replace '["'']', ''
 
-            if ($name.Contains('oci8')) {
-                $name = $Extension
+        if ($name.Contains('oci8')) {
+            $name = $Extension
+        }
+        elseif ($name.Contains('libsodium')) {
+            $name = 'sodium'
+        }
+        elseif ($Extension.Contains('mysql_xdevapi')) {
+            $name = 'mysql_xdevapi'
+        }
+        elseif ([string]$configW32Content -match ($([regex]::Escape($name)) + '\s*=\s*["''](.+?)["'']')) {
+            if ($matches[1] -ne 'no') {
+                $name = $matches[1]
             }
-            elseif ($name.Contains('libsodium')) {
-                $name = 'sodium'
-            }
-            elseif ($Extension.Contains('mysql_xdevapi')) {
-                $name = 'mysql_xdevapi'
-            }
-            elseif ([string]$configW32Content -match ($([regex]::Escape($name)) + '\s*=\s*["''](.+?)["'']')) {
-                if ($matches[1] -ne 'no') {
-                    $name = $matches[1]
-                }
-            }
+        }
 
-            if (-not $patches) {
-                Add-Patches $name
-            }
+        if (-not $patches) {
+            Add-Patches $name
+        }
 
+        if (-not $LocalSrc) {
             Add-BuildLog tick $name "Fetched $name extension"
-            return $name
         }
-        catch {
-            Add-BuildLog cross extension "Failed to fetch extension from $ExtensionUrl"
-            throw
-        }
+
+        return $name
     }
 
     end {}
