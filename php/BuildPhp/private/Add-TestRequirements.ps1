5cf8c33a05cf5869cf419b18a4872dcc5a491107
Maksim Arhipov <93676052+OSPanel@users.noreply.github.com>
2025-12-30 22:34:07 +0300

    Update Add-TestRequirements.ps1

--- php/BuildPhp/private/Add-TestRequirements.ps1
+++ php/BuildPhp/private/Add-TestRequirements.ps1
@@ -32,13 +32,43 @@ function Add-TestRequirements {
         [string] $VsVersion,
         [Parameter(Mandatory = $false, Position=4, HelpMessage='Tests Directory')]
         [ValidateLength(1, [int]::MaxValue)]
-        [string] $TestsDirectory
+        [string] $TestsDirectory,
+        [Parameter(Mandatory = $true, Position=5, HelpMessage='Artifacts Directory')]
+        [ValidateNotNull()]
+        [string] $ArtifactsDirectory
     )
     begin {
     }
     process {
-        Get-PhpBuild -PhpVersion $PhpVersion -Arch $Arch -Ts $Ts -VsVersion $VsVersion
-        Get-PhpTestPack -PhpVersion $PhpVersion -TestsDirectory $TestsDirectory
+        Add-Type -Assembly "System.IO.Compression.Filesystem"
+        $versionInUrl = $PhpVersion
+        if($PhpVersion -eq 'master') {
+            $versionInUrl = "master"
+        }
+        $tsPart = if ($Ts -eq "nts") {"nts-Win32"} else {"Win32"}
+        $binZipFile = "php-$versionInUrl-$tsPart-$VsVersion-$Arch.zip"
+        $testZipFile = "php-test-pack-$versionInUrl.zip"
+
+        $currentDirectory = (Get-Location).Path
+        $binZipFilePath = Join-Path $ArtifactsDirectory $binZipFile
+        $binDirectoryPath = Join-Path $currentDirectory phpbin
+
+        $testZipFilePath = Join-Path $ArtifactsDirectory $testZipFile
+        $testsDirectoryPath = Join-Path $currentDirectory $TestsDirectory
+
+        if(-not(Test-Path $binZipFilePath)) {
+            Write-Host "Downloading PHP build $binZipFile..."
+            Get-PhpBuild -PhpVersion $PhpVersion -Arch $Arch -Ts $Ts -VsVersion $VsVersion
+        } else {
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($binZipFilePath, $binDirectoryPath)
+        }
+
+        if(-not(Test-Path $testZipFilePath)) {
+            Write-Host "Downloading PHP test pack $testZipFile..."
+            Get-PhpTestPack -PhpVersion $PhpVersion -TestsDirectory $TestsDirectory
+        } else {
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($testZipFilePath, $testsDirectoryPath)
+        }
     }
     end {
     }
