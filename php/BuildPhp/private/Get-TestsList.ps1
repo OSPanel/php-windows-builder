bfb10a04e1bf8d59a02baf1962cb1d9fdf224c30
Maksim Arhipov <93676052+OSPanel@users.noreply.github.com>
2025-12-30 23:22:03 +0300

    Update Get-TestsList.ps1

--- php/BuildPhp/private/Get-TestsList.ps1
+++ php/BuildPhp/private/Get-TestsList.ps1
@@ -4,6 +4,8 @@ function Get-TestsList {
         Get the PHP test list.
     .PARAMETER OutputFile
         Output file
+    .PARAMETER Type
+        Test type
     #>
     [OutputType()]
     param (
@@ -11,12 +13,17 @@ function Get-TestsList {
         [ValidateNotNull()]
         [ValidateLength(1, [int]::MaxValue)]
         [string] $OutputFile
+        [string] $OutputFile,
+        [Parameter(Mandatory = $true, Position=1, HelpMessage='Test type')]
+        [ValidateNotNull()]
+        [ValidateSet('php', 'ext')]
+        [string] $Type
     )
     begin {
     }
     process {
         Remove-Item $OutputFile -ErrorAction "Ignore"
-        foreach ($line in Get-Content "$PSScriptRoot\..\config\test_directories") {
+        foreach ($line in Get-Content "$PSScriptRoot\..\config\${Type}_test_directories") {
             $ttr = Get-ChildItem -Path $line -Filter "*.phpt" -Recurse
             foreach ($t in $ttr) {
                 Add-Content $OutputFile ($t | Resolve-Path -Relative)
