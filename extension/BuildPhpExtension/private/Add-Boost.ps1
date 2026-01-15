ce018b008cac44f77de59d4de354fa07efb5ccbe
Maksim Arhipov <93676052+OSPanel@users.noreply.github.com>
2026-01-15 06:43:46 +0300

    Update Add-Boost.ps1

--- extension/BuildPhpExtension/private/Add-Boost.ps1
+++ extension/BuildPhpExtension/private/Add-Boost.ps1
@@ -12,14 +12,11 @@ Function Add-Boost {
         $url = "https://archives.boost.io/release/1.72.0/source/boost_1_72_0.zip"
         Get-File -Url $url -OutFile "boost.zip"
         Expand-Archive -Path "boost.zip" -DestinationPath "../deps"
-        if (-not (Test-Path "../deps/boost")) {
-            New-Item -Path "../deps/boost" -ItemType Directory | Out-Null
-        }
+        Rename-Item -Path "../deps/boost_1_72_0" -NewName "boost"
         if (-not (Test-Path "./boost")) {
             New-Item -Path "./boost" -ItemType Directory | Out-Null
         }
-        Copy-Item -Path "../deps/boost_1_72_0/*" -Destination "../deps/boost" -Recurse
-        Copy-Item -Path "../deps/boost_1_72_0/*" -Destination "./boost" -Recurse
+        Copy-Item -Path "../deps/boost/*" -Destination "./boost" -Recurse
     }
     end {
     }
